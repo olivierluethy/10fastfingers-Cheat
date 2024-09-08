b230368e674bba96ddc74fcf9a70c3d38f9baf7d
Olivier Lüthy <64250640+olivierluethy@users.noreply.github.com>
2024-09-08 14:35:30 +0200

    Make interval for multiplayer randomly

--- multiplayer.js
+++ multiplayer.js
@@ -4,24 +4,34 @@ const inputField = document.querySelector('.interface input');
 
 // If the input field exists, assign the id "inputfield" to it
 if (inputField) {
-    inputField.id = "inputfield";
+  inputField.id = "inputfield";
 }
 
-// Set an interval that executes the following function every 250 milliseconds
-setInterval(() => {
-    // Get the highlighted text by getting the first element with class 'highlight' and getting its text content
-    const highlightedText = document.querySelector('.highlight').textContent;
+function getRandomInterval() {
+  // Generate a random number between 100 (inclusive) and 400 (exclusive)
+  return Math.floor(Math.random() * (800 - 100)) + 100;
+}
 
-    // If highlightedText is not an empty string
-    if (highlightedText) {
-        // Set the focus to the input field
-        inputField.focus();
+// Set an interval that executes the following function with a random interval
+const intervalId = setInterval(() => {
+  // Get the highlighted text by getting the first element with class 'highlight' and getting its text content
+  const highlightedText = document.querySelector('.highlight').textContent;
 
-        // Set the value of the input field to the highlighted text + space
-        inputField.value = highlightedText + ' ';
+  // If highlightedText is not an empty string
+  if (highlightedText) {
+    // Set the focus to the input field
+    inputField.focus();
 
-        // Dispatch an 'input' event to notify the change in the input field
-        const inputEvent = new Event('input', { bubbles: true });
-        inputField.dispatchEvent(inputEvent);
-    }
-}, 250);
+    // Set the value of the input field to the highlighted text + space
+    inputField.value = highlightedText + ' ';
+
+    // Dispatch an 'input' event to notify the change in the input field
+    const inputEvent = new Event('input', { bubbles: true });
+    inputField.dispatchEvent(inputEvent);
+  }
+}, getRandomInterval());
+
+// Optionally, clear the interval when the input field loses focus
+inputField.addEventListener('blur', () => {
+  clearInterval(intervalId);
+});
